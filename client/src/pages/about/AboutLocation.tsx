7f8054b1704499bbb14f73715764df1fef5fc87b
JUNGHO-GIT <junghomun00@gmail.com>
2024-08-31 02:31:28 +0900

    update

--- client/src/pages/about/AboutLocation.tsx
+++ client/src/pages/about/AboutLocation.tsx
@@ -4,52 +4,97 @@ import { useEffect } from "react";
 import { Paper, Grid } from "@imports/ImportMuis.tsx";
 import { Div } from "@imports/ImportComponents.tsx";
 
+// -------------------------------------------------------------------------------------------------
 declare global {
   interface Window {
     naver: any;
   }
 }
 
-const NAVER_MAPS_CLIENT_ID = "piuod7gl89";
-
+// -------------------------------------------------------------------------------------------------
 export const AboutLocation = () => {
+
+  const NAVER_MAPS_CLIENT_ID = process.env.REACT_APP_NAVER_MAPS_CLIENT_ID;
+  const srcPre = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=";
+  const scriptSrc = `${srcPre}${NAVER_MAPS_CLIENT_ID}`;
+
   useEffect(() => {
     const initMap = () => {
-      const location = new window.naver.maps.LatLng(37.5665, 126.9780);
-      const mapOptions = {
-        center: location,
-        zoom: 17,
-      };
+      if (window.naver && window.naver.maps) {
+        const naverMaps = window.naver.maps;
+        const location = new naverMaps.LatLng(37.5666805, 126.9784147);
+        const mapOptions = {
+          center: location,
+          zoom: 17,
+          zoomControl: true,
+          zoomControlOptions: {
+            position: naverMaps.Position.RIGHT_CENTER,
+          },
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: naverMaps.MapTypeControlStyle.BUTTON,
+            position: naverMaps.Position.TOP_RIGHT,
+          },
+        };
+        const newMap = new naverMaps.Map("map", mapOptions);
+        const marker = new naverMaps.Marker({
+          position: location,
+          map: newMap,
+        });
+        const infoWindow = new naverMaps.InfoWindow({
+          content: `
+            <div style="padding: 20px;">
+              <h4>서울특별시청</h4>
+              <p>서울특별시 중구 서소문동 37-9</p>
+            </div>
+          `,
+          backgroundColor: "#fff",
+          borderColor: "#000",
+          borderWidth: 1,
+          anchorSize: new naverMaps.Size(0, 0),
+          anchorSkew: true,
+          anchorColor: "#fff",
+        });
 
-      const map = new window.naver.maps.Map("map", mapOptions);
+        naverMaps.Event.addListener(marker, "click", function () {
+          if (infoWindow.getMap()) {
+            infoWindow.close();
+          }
+          else {
+            infoWindow.open(newMap, marker);
+          }
+        });
 
-      new window.naver.maps.Marker({
-        position: location,
-        map: map,
-      });
+        infoWindow.open(newMap, marker);
+      }
     };
 
     const script = document.createElement("script");
-    script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${NAVER_MAPS_CLIENT_ID}`;
+    script.src = scriptSrc;
     script.async = true;
-    script.onload = () => initMap();
+    script.onload = initMap;
     document.head.appendChild(script);
 
     return () => {
-      script.remove();
+      const script = document.querySelector(`script[src="${scriptSrc}"]`);
+      if (script) {
+        script.remove();
+      }
     };
   }, []);
 
   return (
-      <Paper className={"content-wrapper radius border h-min75vh"}>
-        <Grid container spacing={2}>
-          <Grid size={12}>
-            <h2>가게 위치</h2>
-          </Grid>
-          <Grid size={12}>
-            <Div id={"map"} className={"w-100p h-400"} />
-          </Grid>
+    <Paper className={"content-wrapper radius border h-min75vh"}>
+      <Grid container spacing={2}>
+        <Grid size={12} className={"d-center"}>
+          <Div className={"fs-1-4rem fw-700"}>
+            가게 위치
+          </Div>
         </Grid>
-      </Paper>
+        <Grid size={12}>
+          <Div id={"map"} style={{ width: "100%", height: "400px" }} />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
