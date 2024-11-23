e1c316459357e33ae39de674aea758a0a7a6e29a
JUNGHO-GIT <junghomun00@gmail.com>
2024-11-23 15:26:33 +0900

    2024-11-23 15:26:33

--- client/src/interfaces/components/Img.tsx
+++ client/src/interfaces/components/Img.tsx
@@ -12,11 +12,12 @@ declare type ImgProps = React.HTMLAttributes<HTMLImageElement> & {
   radius?: boolean;
   border?: boolean;
   max?: number;
+  loading?: "eager" | "lazy";
 };
 
 // -------------------------------------------------------------------------------------------------
 export const Img = (
-  { group, src, hover, shadow, radius, border, max, ...props }: ImgProps
+  { group, src, hover, shadow, radius, border, max, loading, ...props }: ImgProps
 ) => {
 
   // 1. common -------------------------------------------------------------------------------------
@@ -70,12 +71,13 @@ export const Img = (
       alt={fileName}
       key={fileName}
       src={imgSrc}
-      loading={"lazy"}
+      loading={loading || "lazy"}
       className={imageClass}
       onError={(e) => {
+        e.currentTarget.style.width = "100%";
+        e.currentTarget.style.height = "100%";
         e.currentTarget.src = `${GCLOUD_URL}/main/empty.webp`;
         e.currentTarget.alt = "empty";
-        e.currentTarget.className = "w-20 h-20";
       }}
     />
   );
